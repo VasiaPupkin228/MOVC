3cf9e40caf25288a1e0445d84c222fa8383e8a2f
artegoser <59178854+artegoser@users.noreply.github.com>
2021-05-04 13:27:45 +0300

    Initial commit

---
+++ options.js
@@ -0,0 +1,6 @@
+module.exports = {
+	port: 80,//9090,
+	ip:"127.0.0.1",//"192.168.0.183",
+	protocol:"http",//"https",
+	public:"localhost"//"https://artegoser.tplinkdns.com:9191"
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const express = require("express");
+const app = express();
+const options = require("./options");
+app.use(express.static(`${process.cwd()}/public`));
+let server;
+
+if(options.protocol === "https"){
+	server = require('https').createServer({
+	  key: fs.readFileSync( './ssl/private.key' , 'utf-8'),
+	  cert: fs.readFileSync( './ssl/certificate.crt', 'utf-8' ),
+	  ca: fs.readFileSync( './ssl/ca_bundle.crt', 'utf-8' ),
+	  csr: fs.readFileSync( './ssl/csr.crt', 'utf-8' )
+	}, app);
+} else{
+	server = require('http').createServer(app);
+}
+
+const io = require('socket.io')(server);
+
+server.listen(options.port, options.ip);
+console.log(`Listening on ${options.ip}:${options.port} or ${options.public}`)
