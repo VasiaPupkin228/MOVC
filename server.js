1e0ce1edda09a10d21c83ca6b9244390cb4a6611
artegoser <59178854+artegoser@users.noreply.github.com>
2021-05-15 13:28:44 +0300

    deprecation

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ app.use(express.static(`${process.cwd()}/public`));
 let server;
 
 if(options.protocol === "https"){
-	server = require('https').createServer({
-	  key: fs.readFileSync( './ssl/private.key' , 'utf-8'),
-	  cert: fs.readFileSync( './ssl/certificate.crt', 'utf-8' ),
-	  ca: fs.readFileSync( './ssl/ca_bundle.crt', 'utf-8' ),
-	  csr: fs.readFileSync( './ssl/csr.crt', 'utf-8' )
-	}, app);
+	//--deprecated on dev--\\
+	// server = require('https').createServer({
+	//   key: fs.readFileSync( './ssl/private.key' , 'utf-8'),
+	//   cert: fs.readFileSync( './ssl/certificate.crt', 'utf-8' ),
+	//   ca: fs.readFileSync( './ssl/ca_bundle.crt', 'utf-8' ),
+	//   csr: fs.readFileSync( './ssl/csr.crt', 'utf-8' )
+	// }, app);
 } else{
 	server = require('http').createServer(app);
 }
 
 const io = require('socket.io')(server);
 
-server.listen(options.port, options.ip);
-console.log(`Listening on ${options.ip}:${options.port} or ${options.public}`)
+server.listen(options.port, options.ip, ()=>{
+	console.log(`Listening on ${options.ip}:${options.port} or ${options.public}`)
+});
