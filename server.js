52200fbea97312f67e292f7415952d07e0311115
artegoser <59178854+artegoser@users.noreply.github.com>
2021-05-22 13:53:50 +0300

    индексирование государств

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const fs = require('fs')
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
-const options = require("./options");
-app.use(express.static(`${process.cwd()}/public`));
-let server;
 
-if(options.protocol === "https"){
-	//--deprecated on dev--\\
-	// server = require('https').createServer({
-	//   key: fs.readFileSync( './ssl/private.key' , 'utf-8'),
-	//   cert: fs.readFileSync( './ssl/certificate.crt', 'utf-8' ),
-	//   ca: fs.readFileSync( './ssl/ca_bundle.crt', 'utf-8' ),
-	//   csr: fs.readFileSync( './ssl/csr.crt', 'utf-8' )
-	// }, app);
-} else{
-	server = require('http').createServer(app);
-}
+app.set("view engine", "ejs");
+app.set("views", `${process.cwd()}/views`)
+app.use("/map",express.static(`${process.cwd()}/public`));
 
-const io = require('socket.io')(server);
+// const io = require('socket.io')(server);
 
-server.listen(options.port, options.ip, ()=>{
-	console.log(`Listening on ${options.ip}:${options.port} or ${options.public}`)
+const PORT = process.env.PORT || 80;
+const URL = process.env.URL || require("./secure.json").url;
+const mongoClient = new MongoClient(URL, { useUnifiedTopology: true });
+mongoClient.connect((err, client)=>{
+    let db = client.db("movc");
+	let co = db.collection("countries");
+	app.get('/countries/:country', (req, res)=>{
+        co.findOne({idc: req.params.country}, (err, val)=>{
+			if(val) res.render("countries", val);
+			else {
+				res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+				res.end("Государство не найдено")
+			}
+		});
+    });
 });
+
+
+app.listen(PORT, ()=>{ console.log(`Listening https on ${PORT}`)});
