809509650b541b96d3f1e973c2ea2b7063b3497e
artegoser <59178854+artegoser@users.noreply.github.com>
2021-05-25 16:31:52 +0300

    oh-sh

--- public/js/prevent.js
+++ public/js/prevent.js
@@ -2,7 +2,7 @@ window.onload = ()=>{
     let input = document.getElementById("idc");
 
     input.oninput = function() {
-        var regexp = /^[a-z\s\-]+$/i;
+        var regexp = /^[a-z\-]+$/i;
         if(!regexp.test(input.value)) {
             input.value = input.value.slice(0,-1);
             return false;
