7427ac4e4b8af40f19e1014591e842e3131ec140
artegoser <59178854+artegoser@users.noreply.github.com>
2021-07-01 19:50:11 +0300

    city markers

--- public/js/map.js
+++ public/js/map.js
@@ -28,24 +28,45 @@ window.onload = async ()=>{
         geo = (await geo.json()).features;
         for(let i = 0; i<geo.length; i++){
                 function onEachFeature(feature, layer) { 
-                        layer.bindPopup(`
-                        <div class="row" style="padding: 5px;">
-                                <div class="col-md-12 col-sm-12" style="padding: 0px;">
-                                        <img class="w-100" src="${country.img}" style="border-radius: 20px 20px 0px 0px;">
-                                </div>
-                                <div class="col-md-12 col-sm-12 text-center" style="border-radius: 0px 0px 20px 20px; background-color: rgb(231, 231, 231)">
-                                        <h5 class="card-title">
-                                                ${country.name}
-                                                <a href="/erth2#marks">
-                                        ${ country.verified === true ?
-                                        `<svg id="completely" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
-                                                <title>Соответствует стандартам ERTH2</title>
-                                        </svg>`: ""}
-                                        </a>
-                                        </h5>
-                                        <a href="countries/${country.idc}" class="btn btn-primary bordert mb-2" style="color:white;">Подробнее</a>
-                                </div>
-                        </div>`);
+                        if(feature.geometry.type==="Polygon")
+                                layer.bindPopup(`
+                                <div class="row" style="padding: 5px;">
+                                        <div class="col-md-12 col-sm-12" style="padding: 0px;">
+                                                <img class="w-100" src="${country.img}" style="border-radius: 20px 20px 0px 0px;">
+                                        </div>
+                                        <div class="col-md-12 col-sm-12 text-center" style="border-radius: 0px 0px 20px 20px; background-color: rgb(231, 231, 231)">
+                                                <h5 class="card-title">
+                                                        ${country.name}
+                                                        <a href="/erth2#marks">
+                                                ${ country.verified === true ?
+                                                `<svg id="completely" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+                                                        <title>Соответствует стандартам ERTH2</title>
+                                                </svg>`: ""}
+                                                </a>
+                                                </h5>
+                                                <a href="countries/${country.idc}" class="btn btn-primary bordert mb-2" style="color:white;">Подробнее</a>
+                                        </div>
+                                </div>`);
+                        else if(feature.geometry.type==="Point"){
+                                if(feature.properties.type==="city"){
+                                        layer.bindPopup(`
+                                                <h5 class="text-center">
+                                                        ${feature.properties.name}
+                                                </h5>
+                                                Население:${feature?.amount}
+                                        `)
+                                }
+                        }
+                }
+                function cpoint(feature, latlng){
+                        if(feature.properties.type==="city"){
+                                let myIcon = L.icon({
+                                        iconSize:     [16, 16],
+                                        iconUrl: 'https://artegoser.github.io/movc/icons/city.png',
+                                });
+                                return L.marker(latlng, { icon: myIcon })
+                        }
+                        return L.marker(latlng)
                 }
                 let country = await fetch('/api/country', {
                         method: 'POST',
@@ -57,6 +78,7 @@ window.onload = async ()=>{
                 country = await country.json();
                 L.geoJSON(geo[i],{
                         onEachFeature: onEachFeature,
+                        pointToLayer: cpoint,
                         style:{
                                 color: geo[i].properties.fill,
                                 weight: 5,
